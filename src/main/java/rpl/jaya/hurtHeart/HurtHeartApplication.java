f4e52363c8643c9c3fb48f91e66791f2ebb6d589
NaufalFikriHakim <naufalfikri2903@gmail.com>
2023-11-27 20:55:02 +0700

    init

---
+++ src/main/java/rpl/jaya/hurtHeart/HurtHeartApplication.java
@@ -0,0 +1,13 @@
+package rpl.jaya.hurtHeart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HurtHeartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HurtHeartApplication.class, args);
+	}
+
+}
---
+++ src/test/java/rpl/jaya/hurtHeart/HurtHeartApplicationTests.java
@@ -0,0 +1,13 @@
+package rpl.jaya.hurtHeart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HurtHeartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
